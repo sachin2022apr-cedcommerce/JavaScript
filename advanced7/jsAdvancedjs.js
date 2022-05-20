0956e37ed87a8cd84e527defe8ba1c0ea24ce417
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-20 14:22:33 +0530

    task7

---
+++ advanced7/jsAdvancedjs.js
@@ -0,0 +1,9 @@
+import { id, name, salary } from "./separate.js";
+
+
+
+let textimp="ID = "+id+",  Name = "+name+",  Salary = "+salary;
+
+document.getElementById("importexport").innerHTML = textimp;
+
+
---
+++ advanced7/separate.js
@@ -0,0 +1,8 @@
+
+const id = "4";
+
+const name = "Jesse";
+
+const salary = 10000;
+
+export {id, name, salary};
