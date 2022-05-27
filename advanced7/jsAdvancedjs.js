c917876dc7bb4d5333efd8b27d28cc540906f3f8
sachin2022apr-cedcommerce <104122223+sachin2022apr-cedcommerce@users.noreply.github.com>
2022-05-27 13:13:13 +0530

    Delete advanced7 directory

--- advanced7/jsAdvancedjs.js
+++
@@ -1,9 +0,0 @@
-import { id, name, salary } from "./separate.js";
-
-
-
-let textimp="ID = "+id+",  Name = "+name+",  Salary = "+salary;
-
-document.getElementById("importexport").innerHTML = textimp;
-
-
--- advanced7/separate.js
+++
@@ -1,8 +0,0 @@
-
-const id = "4";
-
-const name = "Jesse";
-
-const salary = 10000;
-
-export {id, name, salary};
